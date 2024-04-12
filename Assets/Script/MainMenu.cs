e2b8117caf3cc94d40af47a259c9d4d4462b611c
wighthood <zefires@gmail.com>
2024-04-12 12:28:09 +0200

    started working on main menu
    fixed a tag too

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
